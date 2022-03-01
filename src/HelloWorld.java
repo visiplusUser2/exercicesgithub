0e9502a188bf08214321f9d01d700df178967a0e
visiplus <contact@visiplus.fr>
2022-03-01 15:35:40 +0100

    C5 : fond ecran en jaune

--- src/HelloWorld.java
+++ src/HelloWorld.java
@@ -1,6 +1,5 @@
 
-
-
+import java.awt.Color;
 import java.awt.Dimension;
 
 import javax.swing.JFrame;
@@ -9,24 +8,26 @@ import javax.swing.SwingConstants;
 
 public class HelloWorld implements Constants {
 
-
 	public static void main(String[] args) {
 
-        JFrame frame = new JFrame(HELLO_WORLD_JAVA_SWING);
+		JFrame frame = new JFrame(HELLO_WORLD_JAVA_SWING);
 
-        // set frame site
-        frame.setMinimumSize(new Dimension(200, 200));
-        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		// set frame site
+		frame.setMinimumSize(new Dimension(200, 200));
+		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 
-        // center the JLabel
-        JLabel lblText = new JLabel(HELLO_WORLD, SwingConstants.CENTER);
+		// center the JLabel
+		JLabel lblText = new JLabel(HELLO_WORLD, SwingConstants.CENTER);
+		Color yellow = new Color(240, 240, 20);
+		lblText.setOpaque(true);
+		lblText.setBackground(yellow);
 
-        // add JLabel to JFrame
-        frame.getContentPane().add(lblText);
+		// add JLabel to JFrame
+		frame.getContentPane().add(lblText);
 
-        // display it
-        frame.pack();
-        frame.setVisible(true);
+		// display it
+		frame.pack();
+		frame.setVisible(true);
 
-    }
+	}
 }
