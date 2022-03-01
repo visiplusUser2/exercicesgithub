ad7651f5285973063b926f24eb24ec419de07252
visiplus <contact@visiplus.fr>
2022-03-01 15:10:15 +0100

    C2 : extraction des constantes

---
+++ src/Constants.java
@@ -0,0 +1,8 @@
+
+
+public interface Constants {
+	
+	public static final String HELLO_WORLD = "Hello World!";
+	public static final String HELLO_WORLD_JAVA_SWING = "Hello World Java Swing";
+
+}
--- src/HelloWorld.java
+++ src/HelloWorld.java
@@ -1,21 +1,25 @@
 
 
 
-import javax.swing.*;
-import java.awt.*;
+import java.awt.Dimension;
 
-public class HelloWorld {
+import javax.swing.JFrame;
+import javax.swing.JLabel;
+import javax.swing.SwingConstants;
 
-    public static void main(String[] args) {
+public class HelloWorld implements Constants {
 
-        JFrame frame = new JFrame("Hello World Java Swing");
+
+	public static void main(String[] args) {
+
+        JFrame frame = new JFrame(HELLO_WORLD_JAVA_SWING);
 
         // set frame site
         frame.setMinimumSize(new Dimension(800, 600));
         frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 
         // center the JLabel
-        JLabel lblText = new JLabel("Hello World!", SwingConstants.CENTER);
+        JLabel lblText = new JLabel(HELLO_WORLD, SwingConstants.CENTER);
 
         // add JLabel to JFrame
         frame.getContentPane().add(lblText);
